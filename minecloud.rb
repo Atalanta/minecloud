feb9c69d280d4406a296b82258538fafbfe99ffa
Stephen Nelson-Smith <stephen@atalanta-systems.com>
2014-05-16 14:46:38 +0100

    Magi deploy and search plumbed in

--- minecloud.rb
+++ minecloud.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
 require 'sinatra/base'
+require 'magi/api'
+require 'aws-sdk-core'
 
 helpers do
   def play
-    '33.44.55.66'
+    connection = Aws.ec2
+    api = Magi::Api.new
+    ami = api.search(connection, 'minecraft').first[0]
+    @ip = api.deploy(connection, ami)['ip']
   end
 end
 
