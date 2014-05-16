3a9841d9a34513b391f83b5a21c537dd305a09fc
Stephen Nelson-Smith <stephen@atalanta-systems.com>
2014-05-16 13:56:07 +0100

    Initial routing for play and stop

--- minecloud.rb
+++ minecloud.rb
@@ -1,7 +1,21 @@
 require 'sinatra'
-require 'haml'
+require 'sinatra/base'
 
-get '/' do
-  haml :index
+helpers do
+  def play
+    '33.44.55.66'
+  end
 end
 
+get '/' do
+  erb :index
+end
+
+get '/play' do
+  @ip = play
+  erb :play
+end
+
+get '/stop' do
+  erb :stop
+end
